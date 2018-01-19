ca265e1da0536e5ed096a5c087eda04102e5153c
Matthew Wetstein <wetstein@anniegpvm01.fnal.gov>
2018-01-19 15:10:04 -0600

    added my new modules

---
+++ UserTools/LAPPDSave.h
@@ -0,0 +1,29 @@
+#ifndef LAPPDSave_H
+#define LAPPDSave_H
+
+#include <string>
+#include <iostream>
+
+#include "Tool.h"
+
+class LAPPDSave: public Tool {
+
+
+ public:
+
+  LAPPDSave();
+  bool Initialise(std::string configfile,DataModel &data);
+  bool Execute();
+  bool Finalise();
+
+
+ private:
+
+
+
+
+
+};
+
+
+#endif
