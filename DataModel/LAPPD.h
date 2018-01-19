7ded51e54bb900843449f61048c3363b3167014e
Matthew Wetstein <wetstein@anniegpvm01.fnal.gov>
2018-01-19 15:15:46 -0600

    added LAPPD data class

---
+++ DataModel/LAPPD.h
@@ -0,0 +1,27 @@
+#ifndef LAPPD_H
+#define LAPPD_H
+#include <vector>
+
+#include <SerialisableObject.h>
+
+class LAPPD{
+
+  friend class boost::serialization::access;
+
+public:
+
+  std::vector<double> waveform;
+
+  bool Print(){};
+
+ private:
+
+
+  template<class Archive> void serialize(Archive &ar, const unsigned int version){
+
+    ar & waveform;
+
+  }
+
+};
+#endif
