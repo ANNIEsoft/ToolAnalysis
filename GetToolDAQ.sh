eacfa85a99e03042e55a492f9f2d0052594a1941
Ben <b.richards@qmul.ac.uk>
2018-01-22 21:12:48 +0000

    fixing directory issue

--- GetToolDAQ.sh
+++ GetToolDAQ.sh
@@ -57,7 +57,7 @@ then
     wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz
     tar zxf boost_1_60_0.tar.gz
     
-    cd /boost_1_60_0
+    cd boost_1_60_0
     
     mkdir install
     
