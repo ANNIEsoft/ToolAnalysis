4eef8c31f97058fc3da74f0c7aa6d0c1e9d919f1
Matthew Wetstein <wetstein@anniegpvm01.fnal.gov>
2018-01-30 10:39:33 -0600

    actually added the max (and min) fining algo to ANNIEalgo

--- DataModel/ANNIEalgorithms.h
+++ DataModel/ANNIEalgorithms.h
@@ -1,8 +1,10 @@
 #ifndef ANNIEALGORITHMS_H
 #define ANNIEALGORITHMS_H
 
+#include <vector>
+#include <stdlib.h>
 #include <iostream>
 
-double FindPulseMax();
+double FindPulseMax(std::vector<double> *theWav, double &themax, int &maxbin, double &themin, int &minbin);
 
 #endif
