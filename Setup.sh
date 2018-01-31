2586a19e8384ebcdc4b799353bd51c35fe955707
brichards64 <b.richards@qmul.ac.uk.com>
2018-01-31 08:19:48 +0000

    added root lib source in script

--- Setup.sh
+++ Setup.sh
@@ -4,7 +4,7 @@
 
 ToolDAQapp=`pwd`
 
-export LD_LIBRARY_PATH=${ToolDAQapp}/lib:${ToolDAQapp}/ToolDAQ/zeromq-4.0.7/lib:${ToolDAQapp}/ToolDAQ/boost_1_66_0/install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${ToolDAQapp}/lib:${ToolDAQapp}/ToolDAQ/zeromq-4.0.7/lib:${ToolDAQapp}/ToolDAQ/boost_1_66_0/install/lib:${ToolDAQapp}/ToolDAQ/root/lib:$LD_LIBRARY_PATH
 
 for folder in `ls -d ${ToolDAQapp}/UserTools/*/ `
 do
