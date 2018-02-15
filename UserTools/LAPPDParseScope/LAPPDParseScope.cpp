384da65f393b45e86a65d694bd9ca9e3f8316872
wetstein <wetstein@zh-a426a-mba.physics.iastate.edu>
2018-02-15 15:37:44 -0600

    Moved all of the fake pulse generation to LAPPDSim, make more complete use of the ANNIE data structures. Added functionality to the FinPeak routine

--- UserTools/LAPPDParseScope/LAPPDParseScope.cpp
+++ UserTools/LAPPDParseScope/LAPPDParseScope.cpp
@@ -14,16 +14,6 @@ bool LAPPDParseScope::Initialise(std::string configfile, DataModel &data){
   m_data= &data; //assigning transient data pointer
   /////////////////////////////////////////////////////////////////
 
-  // here I would open the input file
-
-  // here I would also store relevant geometry information
-
-  std::string test = "hello";
-  m_data->Stores["ANNIEEvent"]= new BoostStore(false,2);
-  m_data->Stores["ANNIEEvent"]->Header->Set("test",test);
-
-  // initialize the ROOT random number generator
-  myTR = new TRandom3();
 
   return true;
 }
@@ -31,41 +21,15 @@ bool LAPPDParseScope::Initialise(std::string configfile, DataModel &data){
 
 bool LAPPDParseScope::Execute(){
 
-  //create an instance of the Waveform class;
-  Waveform<double> mwav;
+  std::map<int,vector<Waveform<double>>> RawLAPPDData;
 
-  // choose a random amplitude and "time" for the pulse
-  // myTR is declared in the .h file and initialized in Initialise()
-  double theamp = myTR->Gaus(50.,20.); // 50 mV mean, 20 mV sigma
-  double thetime = myTR->Gaus(60.,0.5); // in the 60th sample, with RMS 0.5
+  //loop over N channels, populate
 
-  TF1* aGauss = new TF1("aGauss","gaus",0,256);
-  aGauss->SetParameter(0,theamp); // amplitude of the pulse
-  aGauss->SetParameter(1,thetime); // peak location (in samples)
-  aGauss->SetParameter(2,10.); // width (sigma) of the pulse
 
-  // loop over 256 "psec" samples and fill the Waveform
-  for(int i=0; i<256; i++)
-  {
-    double noise = myTR->Gaus(0.,2.0); //add in random baseline noise (2 mV sig)
-
-    double signal  = aGauss->Eval(i,0,0,0);
-    double thevoltage = signal + noise;
-
-    mwav.PushSample(thevoltage);
-
-    //std::cout<<thevoltage<<std::endl;
-  }
-
-  int thesize = mwav.GetSamples()->size();
-  //std::cout<<"the size here  "<<thesize<<std::endl;
-
-  //put the fake LAPPD pulse into the ANNIEEvent Store, call it "LAPPDtrace"
-  m_data->Stores["ANNIEEvent"]->Set("LAPPDtrace",mwav);
+  m_data->Stores["ANNIEEvent"]->Set("RawLAPPDData",RawLAPPDData);
 
   return true;
 }
-
 
 bool LAPPDParseScope::Finalise(){
 
