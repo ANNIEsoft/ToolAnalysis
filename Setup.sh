87723e4826cf50484273a0a8bafa48536139d6c0
brichards64 <b.richards@qmul.ac.uk>
2018-02-23 22:14:38 +0100

    added source of root thisroot.sh

--- Setup.sh
+++ Setup.sh
@@ -4,6 +4,8 @@
 
 ToolDAQapp=`pwd`
 
+source ${ToolDAQapp}/ToolDAQ/root/bin/thisroot.sh
+
 export LD_LIBRARY_PATH=${ToolDAQapp}/lib:${ToolDAQapp}/ToolDAQ/zeromq-4.0.7/lib:${ToolDAQapp}/ToolDAQ/boost_1_66_0/install/lib:${ToolDAQapp}/ToolDAQ/root/lib:$LD_LIBRARY_PATH
 
 for folder in `ls -d ${ToolDAQapp}/UserTools/*/ `
