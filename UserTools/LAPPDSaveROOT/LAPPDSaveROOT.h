5ec2bf645e6f9d01a449b43aa4560a5a90bdbb96
wetstein <wetstein@wetsteins-MacBook-Air.local>
2018-03-15 12:46:36 -0500

    A basic tool for saving root trees and histos from LAPPD analysis

---
+++ UserTools/LAPPDSaveROOT/LAPPDSaveROOT.h
@@ -0,0 +1,45 @@
+#ifndef LAPPDSaveROOT_H
+#define LAPPDSaveROOT_H
+
+#include <string>
+#include <iostream>
+#include "TFile.h"
+#include "TH1D.h"
+#include "TString.h"
+
+#include "Tool.h"
+
+class LAPPDSaveROOT: public Tool {
+
+
+ public:
+
+  LAPPDSaveROOT();
+  bool Initialise(std::string configfile,DataModel &data);
+  bool Execute();
+  bool Finalise();
+
+
+ private:
+
+   TFile* tf;
+   int miter;
+
+   TTree* outtree;
+   int NChannel;
+   int TrigChannel;
+   int NHistos;
+   TH1D** hAmp;
+   TH1D** hQ;
+   TH1D** hTime;
+   bool isFiltered;
+   bool isIntegrated;
+   int chno;
+   double cfdtime;
+   double amp;
+   double twidth;
+   double Deltat;
+};
+
+
+#endif
