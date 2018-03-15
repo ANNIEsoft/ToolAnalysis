f8ce88c090cce65978e96aaf78a92423342d1b12
wetstein <wetstein@wetsteins-MacBook-Air.local>
2018-03-15 12:43:14 -0500

    completed the basic LAPPD analysis and neutron study code

--- UserTools/NeutronStudyWriteTree/NeutronStudyWriteTree.cpp
+++ UserTools/NeutronStudyWriteTree/NeutronStudyWriteTree.cpp
@@ -25,18 +25,19 @@ bool NeutronStudyWriteTree::Initialise(std::string configfile, DataModel &data){
   outtree->Branch("pi_angle",&piAngle);
   outtree->Branch("pi_E",&piE);
   outtree->Branch("q2",&q2);
-  outtree->Branch("recoE",&recoE);
   outtree->Branch("Nprimaryneutrons",&primneut);
   outtree->Branch("Ntotneutrons",&totneut);
-
   outtree->Branch("mu_angle_smeared",&smearedMuangle);
   outtree->Branch("mu_E_smeared",&smearedMuE);
   outtree->Branch("myrecoE",&myRecoE);
+  outtree->Branch("myrecoE_unsmeared",&myRecoE_unsmeared);
+  outtree->Branch("recoE_orig",&origRecoE);
   outtree->Branch("goodmuon",&isgoodmuon);
   outtree->Branch("isPismeared",&isPismeared);
   outtree->Branch("Ntotneutsmeared",&Ntotneutsmeared);
   outtree->Branch("Nprimneutsmeared",&Nprimneutsmeared);
   outtree->Branch("Nbkgdneutrons",&Nbkgdneutrons);
+  outtree->Branch("Nbkgdneutrons_high",&Nbkgdneutrons_high);
   outtree->Branch("PassedSelection",&passedselection);
   outtree->Branch("MuonEffic",&muonefficiency);
 
@@ -58,17 +59,21 @@ bool NeutronStudyWriteTree::Execute(){
   m_data->Stores["NeutrinoEvent"]->Get("truePionEnergy",piE);
   m_data->Stores["NeutrinoEvent"]->Get("truePionAngle",piAngle);
   m_data->Stores["NeutrinoEvent"]->Get("trueQ2",q2);
-  m_data->Stores["NeutrinoEvent"]->Get("recoNeutrinoEnergy",recoE);
+  m_data->Stores["NeutrinoEvent"]->Get("originalrecoNeutrinoEnergy",origRecoE);
+  m_data->Stores["NeutrinoEvent"]->Get("myrecoNeutrinoEnergy",myRecoE);
+  m_data->Stores["NeutrinoEvent"]->Get("myrecoNuEunsmeared",myRecoE_unsmeared);
   m_data->Stores["NeutrinoEvent"]->Get("smearedMuonAngle",smearedMuangle);
   m_data->Stores["NeutrinoEvent"]->Get("smearedMuonEnergy",smearedMuE);
-  m_data->Stores["NeutrinoEvent"]->Get("myrecoNeutrinoEnergy",myRecoE);
   m_data->Stores["NeutrinoEvent"]->Get("isGoodMuon",isgoodmuon);
   m_data->Stores["NeutrinoEvent"]->Get("smearedIsPion",isPismeared);
   m_data->Stores["NeutrinoEvent"]->Get("smearedNtotalNeutrons",Ntotneutsmeared);
   m_data->Stores["NeutrinoEvent"]->Get("smearedNprimaryNeutrons",Nprimneutsmeared);
   m_data->Stores["NeutrinoEvent"]->Get("NbackgroundNeutrons",Nbkgdneutrons);
+  m_data->Stores["NeutrinoEvent"]->Get("NbackgroundNeutrons_high",Nbkgdneutrons_high);
   m_data->Stores["NeutrinoEvent"]->Get("PassedSelectionCuts",passedselection);
   m_data->Stores["NeutrinoEvent"]->Get("MuonEfficiency",muonefficiency);
+
+
 
   outtree->Fill();
 
