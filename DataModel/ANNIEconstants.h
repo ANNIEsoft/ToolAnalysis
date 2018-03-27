8bbd3a12c8162c008f804fd494485e14cf9e8633
Steven Gardiner <sjgardiner@ucdavis.edu>
2018-03-27 11:15:53 +0200

    Add the ADCCalibrator tool. This tool makes calibrated waveform objects using the raw ADC data.

--- DataModel/ANNIEconstants.h
+++ DataModel/ANNIEconstants.h
@@ -1,6 +1,11 @@
 #ifndef ANNIECONSTANTS_H
 #define ANNIECONSTANTS_H
 
-const double c=3.0;
+/// @brief The impedance to assume when computing charge values for
+/// calibrated ADC hits
+constexpr double ADC_IMPEDANCE = 50.; // Ohm
+
+/// @brief Multiplying by this constant converts ADC counts to Volts
+constexpr double ADC_TO_VOLT = 2.415 / std::pow(2., 12);
 
 #endif
