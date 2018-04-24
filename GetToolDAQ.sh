2b606c9588ba4d53370165c74de9df0acd834753
Ben <b.richards@qmul.ac.uk>
2018-04-24 09:13:02 +0100

    changed to most uptodate version of toolframework

--- GetToolDAQ.sh
+++ GetToolDAQ.sh
@@ -1,44 +1,142 @@
 #!/bin/bash
 
 # rpms prerequisits needs root
-#yum install make gcc-c++ gcc binutils libX11-devel libXpm-devel libXft-devel libXext-devel git 
+#yum install make gcc-c++ gcc binutils libX11-devel libXpm-devel libXft-devel libXext-devel git bzip2-devel python-devel
 
+init=1
+tooldaq=1
+boostflag=1
+zmq=1
+final=1
 
-mkdir ToolDAQ
+while [ ! $# -eq 0 ]
+do
+    case "$1" in
+	--help | -h)
+	    helpmenu
+	    exit
+	    ;;
+		
+	--no_boost | -b)
+            echo "Installing ToolDAQ without boost"
+            boostflag=0
+	    ;;
+	
+	--no_init )
+	     echo "Installing ToolDAQ without creating ToolDAQ Folder"
+	    init=0;
+	    ;;
+
+	--no_zmq )
+            echo "Installing ToolDAQ without zmq"
+            zmq=0
+            ;;
+
+	--no_tooldaq )
+	    echo "Installing dependancies without ToolDAQ"
+	    tooldaq=0
+	    ;;
+
+	--no_final )
+            echo "Installing ToolDAQ without compiling ToolAnalysis"
+            final=0
+            ;;
+
+	--ToolDAQ_ZMQ )
+            echo "Installing ToolDAQ & ZMQ"
+	    rootflag=0
+	    boostflag=0
+	    final=0
+	    MrdTrackLib=0
+	    WCSimlib=0   
+            ;;
+
+	--Boost )
+            echo "Installing Boost"
+	    init=0
+	    tooldaq=0
+	    rootflag=0
+	    zmq=0
+	    final=0
+	    MrdTrackLib=0
+	    WCSimlib=0
+            ;;
+	
+	
+	--Final )
+            echo "Compiling ToolDAQ"
+	    init=0
+	    tooldaq=0
+	    rootflag=0
+	    boostflag=0
+	    zmq=0
+	    MrdTrackLib=0
+	    WCSimlib=0
+            ;;
+
+    esac
+    shift
+done
+
+if [ $init -eq 1 ]
+then
+    
+    mkdir ToolDAQ
+fi
+
 cd ToolDAQ
 
-git clone --branch V2.0 https://github.com/ToolDAQ/ToolDAQFramework.git
-git clone https://github.com/ToolDAQ/zeromq-4.0.7.git
-wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz
-tar zxf boost_1_60_0.tar.gz
+if [ $tooldaq -eq 1 ]
+then
 
-cd zeromq-4.0.7
+git clone https://github.com/ToolDAQ/ToolDAQFramework.git
+fi
 
-./configure --prefix=`pwd`
-make
-make install
+if [ $zmq -eq 1 ]
+then
+    git clone https://github.com/ToolDAQ/zeromq-4.0.7.git
+    
+    cd zeromq-4.0.7
+    
+    ./configure --prefix=`pwd`
+    make -j8
+    make install
+    
+    export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+    
+    cd ../
+fi
 
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+if [ $boostflag -eq 1 ]
+then
+    
+    
+    wget http://downloads.sourceforge.net/project/boost/boost/1.66.0/boost_1_66_0.tar.gz
+    tar zxf boost_1_66_0.tar.gz
+    rm -rf boost_1_66_0.tar.gz
+    
+    cd boost_1_66_0
+    
+    mkdir install
+    
+    ./bootstrap.sh --prefix=`pwd`/install/  > /dev/null 2>/dev/null
+    ./b2 install iostreams
+    
+    export LD_LIBRARY_PATH=`pwd`/install/lib:$LD_LIBRARY_PATH
+    cd ../
+fi
 
-cd ../boost_1_60_0
+cd ../
 
-mkdir install
 
-./bootstrap.sh --prefix=`pwd`/install/  > /dev/null 2>/dev/null
-./b2 install iostreams
 
-export LD_LIBRARY_PATH=`pwd`/install/lib:$LD_LIBRARY_PATH
-
-cd ../ToolDAQFramework
-
-make clean
-make
-
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-
-cd ../../
-
-make clean
-make
-
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+if [ $final -eq 1 ]
+then
+    
+    echo "current directory"
+    echo `pwd`
+    make clean
+    make 
+    
+    export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+fi
