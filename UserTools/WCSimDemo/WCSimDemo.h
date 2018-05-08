38eac98a1585840ec1b1e6846bdc175867f78dec
Marcus O'Flaherty <moflaher@fnal.gov>
2018-05-07 19:44:19 -0500

    Add WCSimDemo tool showing use of LoadWCSim for truth analysis

---
+++ UserTools/WCSimDemo/WCSimDemo.h
@@ -0,0 +1,57 @@
+/* vim:set noexpandtab tabstop=4 wrap */
+#ifndef WCSimDemo_H
+#define WCSimDemo_H
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "Tool.h"
+#include "ChannelKey.h"
+#include "Particle.h"
+#include "Hit.h"
+#include "LAPPDHit.h"
+#include "TriggerClass.h"
+#include "TimeClass.h"
+
+class WCSimDemo: public Tool {
+	
+	public:
+	
+	WCSimDemo();
+	bool Initialise(std::string configfile,DataModel &data);
+	bool Execute();
+	bool Finalise();
+	
+	private:
+	
+	int verbosity=0;
+	std::string inputfile;
+	unsigned long NumEvents;
+	
+	// contents of ANNIEEvent filled by LoadWCSim and LoadWCSimLAPPD
+	std::string MCFile;
+	uint32_t RunNumber;       // retrieved from MC file but simulations tend to only ever be run 0. 
+	uint32_t SubrunNumber;    // MC has no 'subrun', always 0
+	uint32_t EventNumber;     // flattens the 'event -> trigger' MC hierarchy
+	uint64_t MCEventNum;      // event number in MC file
+	uint16_t MCTriggernum;    // trigger number in MC file
+	
+	std::vector<MCParticle>* MCParticles=nullptr;                      // truth tracks
+	std::map<ChannelKey,std::vector<Hit>>* MCHits=nullptr;             // PMT hits
+	std::map<ChannelKey,std::vector<LAPPDHit>>* MCLAPPDHits=nullptr;   // LAPPD hits
+	std::map<ChannelKey,std::vector<Hit>>* TDCData=nullptr;            // MRD & veto hits
+	TimeClass* EventTime=nullptr;    // NDigits trigger time in ns from when the particles were generated
+	
+	// verbosity levels: if 'verbosity' < this level, the message type will be logged.
+	int v_error=0;
+	int v_warning=1;
+	int v_message=2;
+	int v_debug=3;
+	std::string logmessage;
+	int get_ok;
+	
+};
+
+
+#endif
