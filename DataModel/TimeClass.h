11f92abac2ce1d91f8626f67654911915fd77f3f
root <Seand1@iastate.edu>
2018-05-23 17:33:29 +0200

    added psec into timeclass

--- DataModel/TimeClass.h
+++ DataModel/TimeClass.h
@@ -8,35 +8,39 @@
 using namespace std;
 
 class TimeClass : public SerialisableObject{
-	
+
 	friend class boost::serialization::access;
-	
+
 	public:
-  TimeClass() : unixns(0) {serialise=true;}
-  TimeClass(uint64_t timens) : unixns(timens) {serialise=true;}
-	
+  TimeClass() : unixns(0) , psec(0) {serialise=true;}
+  TimeClass(uint64_t timens) : unixns(timens) , psec(0) {serialise=true;}
+	TimeClass(uint64_t timens , double psc) : unixns(timens) , psec(psc) {serialise=true;}
+
 	inline uint64_t GetNs() const {return unixns;}
+	inline double GetPsec() const {return psec;}
 	inline void SetNs(uint64_t tns){unixns=tns;}
-	
+	inline void SetPsec(double psc){psec=psc;}
+
 	bool Print() {
 		cout<<"unixns : "<<unixns<<endl;
-		
+
 		return true;
 	}
-	
+
 	private:
 	uint64_t unixns;
-	
+  double psec;
 	template<class Archive> void serialize(Archive & ar, const unsigned int version){
 		if(serialise){
 			ar & unixns;
 		}
 	}
-	
+
 	void Clear(){
 		unixns = 0;
+		psec = 0;
 	}
-	
+
 };
 
 // Operator for printing the time since the Unix epoch (in ns) represented by
