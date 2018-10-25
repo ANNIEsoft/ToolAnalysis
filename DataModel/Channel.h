98973be526fd77fa8fea624a89f6d496fb365d7f
root <seand1@iastate.edu>
2018-10-25 20:31:34 +0200

    added status to channel class

--- DataModel/Channel.h
+++ DataModel/Channel.h
@@ -13,46 +13,45 @@ class Channel : public SerialisableObject{
 	friend class boost::serialization::access;
 
 	public:
-		Channel() : ChannelKey(), relx(), rely(), relz(), card(), crate(), elecid(), stripside(), rotorientation() {serialise=true;}
-		Channel(unsigned long chnky, float xpos, float ypos, float zpos ,unsigned int crd, unsigned int crt, unsigned int elcid, int strp, int rotation) : ChannelKey(chnky), relx(xpos), rely(ypos), relz(zpos), card(crd), crate(crt), elecid(elcid), stripside(strp), rotorientation(rotation) {serialise=true;}
+		Channel() : ChannelKey(), RelPara(), RelPerp(), card(), crate(), elecid(), stripside(), rotorientation(), status() {serialise=true;}
+		Channel(unsigned long chnky, float relpara, float relperp, unsigned int crd, unsigned int crt, unsigned int elcid, int strp, int rotation, int chanstat) : ChannelKey(chnky), RelPara(relpara), RelPerp(relperp), card(crd), crate(crt), elecid(elcid), stripside(strp), rotorientation(rotation), status(chanstat) {serialise=true;}
 
 unsigned long GetChannelKey(){return ChannelKey;}
-float GetRelXPos(){return relx;} //relative to position of detector 
-float GetRelYPos(){return rely;}
-float GetRelZPos(){return relz;}
+float GetRelPara(){return RelPara;} //relative to position of detector
+float GetRelPerp(){return RelPerp;}
 int GetStripSide(){return stripside;}
 unsigned int GetCard(){return card;}
 unsigned int GetCrate(){return crate;}
 unsigned int GetElecID(){return elecid;}
 int GetRotOrientation(){return rotorientation;}
-
+int GetStatus(){return status;}
 void SetChannelKey(unsigned long channelkeyin){ChannelKey=channelkeyin;}
-void SetRelPos(int xin,int yin,int zin){ relx=xin; rely=yin; relz=zin;}
+void SetRelPos(int para,int perp){ RelPara=para; RelPerp=perp;}
 void SetStripSide(int stripsidein){stripside=stripsidein;}
 void SetCard(unsigned int cardin){card=cardin;}
 void SetCrate(unsigned int cratein){crate=cratein;}
 void SetElecID(unsigned int elecidin){elecid=elecidin;}
 void SetRotOrientation(int rotorin){rotorientation=rotorin;}
-
+void SetStatus(int stat){status=stat;}
 
 	private:
 		unsigned long ChannelKey;
-		float relx, rely, relz;
+		float RelPara, RelPerp;
 		int stripside; //-1 for left, 1 for right
 		unsigned int card, crate, elecid;
 		int rotorientation;
-
+		int status;
 	template<class Archive> void serialize(Archive & ar, const unsigned int version){
 		if(serialise){
 			ar & ChannelKey;
-			ar & relx;
-			ar & rely;
-			ar & relz;
+			ar & RelPara;
+			ar & RelPerp;
 			ar & stripside;
 			ar & card;
 			ar & crate;
 			ar & elecid;
 			ar & rotorientation;
+			ar & status;
 		}
 	}
 };
