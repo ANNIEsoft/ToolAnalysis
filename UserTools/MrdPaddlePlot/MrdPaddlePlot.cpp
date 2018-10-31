393b8ff0af7a2c8b78f0830d45bfeb517a5a880d
Marcus O'Flaherty <msoflaherty1@sheffield.ac.uk>
2018-10-31 22:13:49 +0000

    remove preprocessor disable for OGL 3D MRD track viewer. shift config enable from PlotMrdTracksConfig to ToolsConfig

--- UserTools/MrdPaddlePlot/MrdPaddlePlot.cpp
+++ UserTools/MrdPaddlePlot/MrdPaddlePlot.cpp
@@ -1,6 +1,7 @@
 /* vim:set noexpandtab tabstop=4 wrap */
 #include "MrdPaddlePlot.h"
 #include "TCanvas.h"
+#define GOT_EVE 1
 #ifdef GOT_EVE
 #include "TGeoManager.h"
 #include "TEveLine.h"
@@ -77,7 +78,7 @@ bool MrdPaddlePlot::Initialise(std::string configfile, DataModel &data){
 		hdigittimes = new TH1D("hdigittimes","MRD Track Digit Times",100,0,1000);
 	}
 	
-#ifdef GOT_GEO
+#ifdef GOT_EVE
 	if(drawGdmlOverlay){
 		// Import the gdml geometry for the detector:
 		TGeoManager::Import(gdmlpath.c_str());
@@ -345,8 +346,8 @@ bool MrdPaddlePlot::Execute(){
 		}
 		
 		//gSystem->ProcessEvents();
-		//gPad->WaitPrimitive();
-		std::this_thread::sleep_for (std::chrono::seconds(2));
+		//if(enableTApplication) gPad->WaitPrimitive();
+		if(enableTApplication) std::this_thread::sleep_for (std::chrono::seconds(2));
 		
 		//if(earlyexit) break;
 	} // end loop over subevents
