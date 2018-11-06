29d79ee570fb1600ae633248c17937c9d68b38eb
Marcus O'Flaherty <msoflaherty1@sheffield.ac.uk>
2018-11-06 22:18:57 +0000

    prevent autoloading warnings by passing path to headers for ROOT custom classes

--- SetupFNAL.sh
+++ SetupFNAL.sh
@@ -9,7 +9,7 @@ TOOLANALYSIS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /cvmfs/annie.opensciencegrid.org/setup_annie.sh --local
 
 export LD_LIBRARY_PATH=${TOOLANALYSIS_ROOT}/lib:${TOOLANALYSIS_ROOT}/ToolDAQ/ToolDAQFramework/lib/:${TOOLANALYSIS_ROOT}/ToolDAQ/zeromq-4.0.7/lib:${TOOLANALYSIS_ROOT}/ToolDAQ/WCSimLib:${TOOLANALYSIS_ROOT}/ToolDAQ/MrdTrackLib/src:$LD_LIBRARY_PATH
-export ROOT_INCLUDE_PATH=${TOOLANALYSIS_ROOT}/ToolDAQ/WCSimLib/include/:$ROOT_INCLUDE_PATH
+export ROOT_INCLUDE_PATH=${TOOLANALYSIS_ROOT}/ToolDAQ/WCSimLib/include/:${TOOLANALYSIS_ROOT}/ToolDAQ/MrdTrackLib/include:$ROOT_INCLUDE_PATH
 
 for folder in `ls -d ${TOOLANALYSIS_ROOT}/UserTools/*/ `
 do
