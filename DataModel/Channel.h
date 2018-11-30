64b34c000004b0be098394df2fc2ae0fac6ac5b4
root <seand1@iastate.edu>
2018-11-30 17:13:11 +0100

    Channel class added and made functional

--- DataModel/Channel.h
+++ DataModel/Channel.h
@@ -1,56 +1,64 @@
-/* vim:set noexpandtab tabstop=4 wrap */
-#ifndef CHANNELCLASS_H
-#define CHANNELCLASS_H
+#ifndef CHANNEL_H
+#define CHANNEL_H
 
-#include<SerialisableObject.h>
-#include "Position.h"
-
-using namespace std;
-
+#include <SerialisableObject.h>
 
 class Channel : public SerialisableObject{
 
-	friend class boost::serialization::access;
+  friend class boost::serialization::access;
 
-	public:
-		Channel() : ChannelKey(), channelrelposition(), card(), crate(), elecid(), stripside(), status() {serialise=true;}
-		Channel(unsigned long chnky, Position chanpos, unsigned int crd, unsigned int crt, unsigned int elcid, int strp, int chanstat) : ChannelKey(chnky), channelrelposition(chanpos), card(crd), crate(crt), elecid(elcid), stripside(strp), status(chanstat) {serialise=true;}
+ public:
 
-unsigned long GetChannelKey(){return ChannelKey;}
-Position GetChannelPosition(){return channelrelposition;}
-int GetStripSide(){return stripside;}
-unsigned int GetCard(){return card;}
-unsigned int GetCrate(){return crate;}
-unsigned int GetElecID(){return elecid;}
-
-int GetStatus(){return status;}
+  Channel() : ChannelID(), channelrelposition(), card(), crate(), elecid(), stripside(), status() {serialise=true;}
+  Channel(unsigned long chnID, Position chanpos, unsigned int crd, unsigned int crt, unsigned int elcid, int strp, int chanstat) : ChannelID(chnID), channelrelposition(chanpos), card(crd), crate(crt), elecid(elcid), stripside(strp), status(chanstat)  {serialise=true;}
 
 
-void SetChannelKey(unsigned long channelkeyin){ChannelKey=channelkeyin;}
-void SetRelPos(Position pos){ channelrelposition=pos;}
-void SetStripSide(int stripsidein){stripside=stripsidein;}
-void SetCard(unsigned int cardin){card=cardin;}
-void SetCrate(unsigned int cratein){crate=cratein;}
-void SetElecID(unsigned int elecidin){elecid=elecidin;}
-void SetStatus(int stat){status=stat;}
+  unsigned long GetChannelID(){return ChannelID;}
+  Position GetChannelPosition(){return channelrelposition;}
+  int GetStripSide(){return stripside;}
+  unsigned int GetCard(){return card;}
+  unsigned int GetCrate(){return crate;}
+  unsigned int GetElecID(){return elecid;}
+  int GetStatus(){return status;}
 
-	private:
-		unsigned long ChannelKey;
-		int stripside; //-1 for left, 1 for right
-		unsigned int card, crate, elecid;
-		int status;
-		Position channelrelposition;
-	template<class Archive> void serialize(Archive & ar, const unsigned int version){
-		if(serialise){
-			ar & ChannelKey;
-			ar & channelrelposition;
-			ar & stripside;
-			ar & card;
-			ar & crate;
-			ar & elecid;
-			ar & status;
-		}
-	}
+
+  void SetChannelID(unsigned long channelIDin){ChannelID=channelIDin;}
+  void SetRelPos(Position pos){ channelrelposition=pos;}
+  void SetStripSide(int stripsidein){stripside=stripsidein;}
+  void SetCard(unsigned int cardin){card=cardin;}
+  void SetCrate(unsigned int cratein){crate=cratein;}
+  void SetElecID(unsigned int elecidin){elecid=elecidin;}
+  void SetStatus(int stat){status=stat;}
+
+bool Print() {
+
+
+
+return true;
+}
+
+ private:
+
+   unsigned long ChannelID;
+   int stripside;
+   unsigned int card, crate, elecid;
+   int status;
+   Position channelrelposition;
+  template<class Archive> void serialize(Archive & ar, const unsigned int  version){
+
+
+    if(serialise){
+      ar & ChannelID;
+      ar & channelrelposition;
+      ar & stripside;
+      ar & card;
+      ar & crate;
+      ar & elecid;
+      ar & status;
+    }
+  }
+
+
 };
 
 #endif
