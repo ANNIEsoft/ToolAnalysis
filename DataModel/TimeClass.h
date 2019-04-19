bacb9b0e08f42d2dc9f365d443f422517638c5df
Marcus O'Flaherty <msoflaherty1@sheffield.ac.uk>
2019-04-19 15:34:12 +0100

    Add AsString method to TimeClass to convert to a human-readable date/time string. Silence compiler warnings about char* conversions and uninitialized values in multiple tools.

--- DataModel/TimeClass.h
+++ DataModel/TimeClass.h
@@ -3,9 +3,13 @@
 #define TIMECLASS_H
 
 #include <iostream>
+#include <time.h>
 #include<SerialisableObject.h>
 
 using namespace std;
+namespace {
+	constexpr uint64_t NS_TO_SECONDS = 1000000000;
+}
 
 class TimeClass : public SerialisableObject{
 	
@@ -19,9 +23,34 @@ class TimeClass : public SerialisableObject{
 	inline void SetNs(uint64_t tns){unixns=tns;}
 	
 	bool Print() {
-		cout<<"unixns : "<<unixns<<endl;
-		
+		cout<< AsString() << endl;
 		return true;
+	}
+	
+	std::string AsString(){
+		// convert to human readable format
+		uint64_t rounded_seconds = static_cast<uint64_t>(floor(unixns/NS_TO_SECONDS));
+		time_t rawtime = rounded_seconds;
+		uint64_t remaining_ns = unixns - rounded_seconds*NS_TO_SECONDS;
+		struct tm * ptm;               // time structure from which components can be retrieved
+		ptm = gmtime(&rawtime);        // fill timestructure with UTC time
+		struct tm tm_mb = tm(*ptm);    // the struct filled by gmtime is internal, so gets overridden
+							           // by subsequent calls. We need to make a copy.
+		
+		int bufsize=100;
+		char logbuffer[bufsize];
+		int discardedcharcount = snprintf(logbuffer, bufsize,
+			"%d/%d/%d %2d:%2d:%2d.%09lu UTC (%lu NS since Unix Epoch)",
+			tm_mb.tm_year+1900,tm_mb.tm_mon+1,tm_mb.tm_mday,tm_mb.tm_hour,tm_mb.tm_min,
+			tm_mb.tm_sec,remaining_ns,unixns); // month+1 because it's "months since january"
+		
+		std::string timestamp_as_string(logbuffer);
+		return timestamp_as_string;
+	}
+	
+	void AsString(std::string& stringin){
+		stringin=AsString();
+		return;
 	}
 	
 	private:
