e501ef7841ff234fcd62e1e6b2cb846c217f2e85
Teal Pershing <pershint@github.com>
2019-05-17 23:55:33 +0200

    Method in place for loading geometry initialization details.  Method for loading MRDs in progress

--- UserTools/LoadGeometry/LoadGeometry.cpp
+++ UserTools/LoadGeometry/LoadGeometry.cpp
@@ -20,8 +20,7 @@ bool LoadGeometry::Initialise(std::string configfile, DataModel &data){
   m_variables.Get("VetoPMTGeoFile", fVetoPMTGeoFile);
   m_variables.Get("LAPPDGeoFile", fLAPPDGeoFile);
   m_variables.Get("DetectorGeoFile", fDetectorGeoFile);
-  //Check files exist
-  if(!this->FileExists(fFACCMRDGeoFile)){
+  //Check files exist if(!this->FileExists(fFACCMRDGeoFile)){
 		Log("LoadGeometry Tool: File for FACC/MRD Geomtry does not exist!",v_error,verbosity); 
 		return false;
   }
@@ -42,10 +41,82 @@ bool LoadGeometry::Finalise(){
 }
 
 void LoadGeometry::ConstructGeometry(){
-  int GeometryVersion = 1;
+  //Initialize the geometry using the geometry CSV file entries 
+  this->InitializeGeometry();
 
+  //Load MRD Geometry Detector/Channel Information
+  this->LoadFACCMRDDetectors();
+}
+
+void LoadGeometry::LoadFACCMRDDetectors(){
   //First, get the MRD file data key
-  std::string MRDKey = this->GetKeyLine(fFACCMRDGeoFile);
+  std::string MRDLegend = this->GetLegendLine(fFACCMRDGeoFile);
+  std::vector<std::string> LegendEntries;
+  boost::split(LegendEntries,MRDLegend, boost::is_any_of(","), boost::token_compress_on); 
+ 
+  std::string line;
+  ifstream myfile(fDetectorGeoFile.c_str());
+  if (myfile.is_open()){
+    //First, get to where data starts
+    while(getline(myfile,line)){
+      if(line.find("#") continue;
+      if(line.find(DataStartLineLabel){
+        break;
+      }
+    }
+    //Loop over lines, collect all detector data
+    while(getline(myfile,line)){
+      std::cout << line << std::endl; //has our stuff;
+      if(line.find("#") continue;
+      if(line.find(DataEndLineLabel)) break;
+      std::vector<std::string> DataEntries;
+      //Parse data line, make corresponding detector/channel
+      Detector* FACCMRDDetector = this->ParseMRDDataEntry(DataEntries);
+  } else {
+    Log("LoadGeometry tool: Something went wrong opening a file!!!",v_error,verbosity);
+  }
+  Log("LoadGeometry tool: Legend line label not found!!!",v_error,verbosity);
+}
+
+Detector* LoadGeometry::ParseMRDDataEntry(std::vector<std::string> DataEntries){
+  //Parse the line for information needed to fill the detector & channel classes
+		Detector adet(uniquedetectorkey,
+					  "MRD",
+					  CylLocString,
+					  Position( apmt.GetPosition(0)/100.,
+					            apmt.GetPosition(1)/100.,
+					            apmt.GetPosition(2)/100.),
+					  Direction(apmt.GetOrientation(0),
+					            apmt.GetOrientation(1),
+					            apmt.GetOrientation(2)),
+					  apmt.GetName(),
+					  detectorstatus::ON,
+					  0.);
+
+		Channel pmtchannel( uniquechannelkey,
+							Position(0,0,0.),
+							0, // stripside
+							0, // stripnum
+							TDC_Crate_Num,
+							TDC_Card_Num,
+							TDC_Chan_Num,
+							-1,                 // TDC has no level 2 signal handling
+							-1,
+							-1,
+							LeCroy_HV_Crate_Num,
+							LeCroy_HV_Card_Num,
+							LeCroy_HV_Chan_Num,
+							channelstatus::ON);
+		
+		// Add this channel to the geometry
+		if(verbosity>4) cout<<"Adding channel "<<uniquechannelkey<<" to detector "<<uniquedetectorkey<<endl;
+		adet.AddChannel(pmtchannel);
+}
+void LoadGeometry::InitializeGeometry(){
+  //Get the Detector file data key
+  std::string DetectorLegend = this->GetLegendLine(fDetectorGeoFile);
+  std::vector<std::string> LegendEntries;
+  boost::split(LegendEntries,DetectorLegend, boost::is_any_of(","), boost::token_compress_on); 
  
   //Initialize at zero; will be set later after channels are loaded 
   int numtankpmts = 0;
@@ -53,40 +124,71 @@ void LoadGeometry::ConstructGeometry(){
   int nummrdpmts = 0; 
   int numvetopmts = 0;
 
-  //units in meters
-  double tank_xcenter = 0; 
-  double tank_ycenter = 0; 
-  double tank_zcenter = 0;
-  Position tank_center(tank_xcenter, tank_ycenter, tank_zcenter);
-  double tank_radius = 0;
-  double tank_halfheight = 0;
-  //Currently hard-coded; estimated with a tape measure on the ANNIE frame :)
-  double pmt_enclosed_radius = 1.0;
-  double pmt_enclosed_halfheight = 1.45;
-  // geometry variables are in MRDSpecs.hh
-  double mrd_width  =  (MRDSpecs::MRD_width)  / 100.;
-  double mrd_height =  (MRDSpecs::MRD_height) / 100.;
-  double mrd_depth  =  (MRDSpecs::MRD_depth)  / 100.;
-  double mrd_start  =  (MRDSpecs::MRD_start)  / 100.;
-  if(verbosity>1) cout<<"we have "<<numtankpmts<<" tank pmts, "<<nummrdpmts
-  				  <<" mrd pmts and "<<numlappds<<" lappds"<<endl;
-  
-  // Initialize the Geometry
-  Geometry* AnnieGeometry = new Geometry(GeometryVersion,
-                                         tank_center,
-                                         tank_radius,
-                                         tank_halfheight,
-                                         pmt_enclosed_radius,
-                                         pmt_enclosed_halfheight,
-                                         mrd_width,
-                                         mrd_height,
-                                         mrd_depth,
-                                         mrd_start,
-                                         numtankpmts,
-                                         nummrdpmts,
-                                         numvetopmts,
-                                         numlappds,
-                                         geostatus::FULLY_OPERATIONAL);
+  //Initialize data that will be fed to Geometry (units in meters)
+  int geometry_version;
+  double tank_xcenter,tank_ycenter,tank_zcenter; 
+  double tank_radius,tank_halfheight, pmt_enclosed_radius, pmt_enclosed_halfheight;
+  double mrd_width,mrd_height,mrd_depth,mrd_start;
+
+  std::string line;
+  ifstream myfile(fDetectorGeoFile.c_str());
+  if (myfile.is_open()){
+    //First, get to where data starts
+    while(getline(myfile,line)){
+      if(line.find("#") continue;
+      if(line.find(DataStartLineLabel){
+        break;
+      }
+    }
+    //Loop over lines, collect all detector data (should only be one line here)
+    while(getline(myfile,line)){
+      std::cout << line << std::endl; //has our stuff;
+      if(line.find("#") continue;
+      if(line.find(DataEndLineLabel)) break;
+      std::vector<std::string> DataEntries;
+      boost::split(DataEntries,line, boost::is_any_of(","), boost::token_compress_on); 
+      for (int i=0; i<DataEntries.size(); i++){
+        //Check Legend at i, load correct data type
+        int ivalue;
+        double dvalue;
+        if(LegendEntries.at(i) == "geometry_version") ivalue = std:stoi(DataEntries.at(i));
+        else dvalue = std::stod(DataEntries.at(i);
+        if (LegendEntries.at(i) == "geometry_version") geometry_version = ivalue;
+        if (LegendEntries.at(i) == "tank_xcenter") tank_xcenter = dvalue;
+        if (LegendEntries.at(i) == "tank_ycenter") tank_ycenter = dvalue;
+        if (LegendEntries.at(i) == "tank_zcenter") tank_zcenter = dvalue;
+        if (LegendEntries.at(i) == "tank_radius") tank_radius = dvalue;
+        if (LegendEntries.at(i) == "tank_halfheight") tank_halfheight = dvalue;
+        if (LegendEntries.at(i) == "pmt_enclosed_radius") pmt_enclosed_radius = dvalue;
+        if (LegendEntries.at(i) == "pmt_enclosed_halfheight") pmt_enclosed_halfheight = dvalue;
+        if (LegendEntries.at(i) == "mrd_width") mrd_width = dvalue;
+        if (LegendEntries.at(i) == "mrd_height") mrd_height = dvalue;
+        if (LegendEntries.at(i) == "mrd_depth") mrd_depth = dvalue;
+        if (LegendEntries.at(i) == "mrd_start") mrd_start = dvalue;
+      } 
+    }
+    Position tank_center(tank_xcenter, tank_ycenter, tank_zcenter);
+    // Initialize the Geometry
+    AnnieGeometry = new Geometry(GeometryVersion,
+                                 tank_center,
+                                 tank_radius,
+                                 tank_halfheight,
+                                 pmt_enclosed_radius,
+                                 pmt_enclosed_halfheight,
+                                 mrd_width,
+                                 mrd_height,
+                                 mrd_depth,
+                                 mrd_start,
+                                 numtankpmts,
+                                 nummrdpmts,
+                                 numvetopmts,
+                                 numlappds,
+                                 geostatus::FULLY_OPERATIONAL);
+  } else {
+    Log("LoadGeometry tool: Something went wrong opening a file!!!",v_error,verbosity);
+  }
+  Log("LoadGeometry tool: Legend line label not found!!!",v_error,verbosity);
+}
 
 bool LoadGeometry::FileExists(const std::string& name) {
   ifstream myfile(name.c_str());
@@ -94,14 +196,13 @@ bool LoadGeometry::FileExists(const std::string& name) {
 }
 
 
-std::string DigitBuilder::GetKeyLine(const std::string& name) {
-  std::string KeyLineLabel = "KEY_LINE";
+std::string DigitBuilder::GetLegendLine(const std::string& name) {
   std::string line;
   ifstream myfile(name.c_str());
   if (myfile.is_open()){
     while(getline(myfile,line)){
       if(line.find("#") continue;
-      if(line.find(KeyLineLabel){
+      if(line.find(LegendLineLabel){
         //Next line is the title line
         getline(myfile,line);
         return line;
@@ -110,8 +211,7 @@ std::string DigitBuilder::GetKeyLine(const std::string& name) {
   } else {
     Log("LoadGeometry tool: Something went wrong opening a file!!!",v_error,verbosity);
   }
-  Log("LoadGeometry tool: Key line label not found!!!",v_error,verbosity);
+  Log("LoadGeometry tool: Legend line label not found!!!",v_error,verbosity);
   return "null";
-
 }
 
--- UserTools/LoadGeometry/LoadGeometry.h
+++ UserTools/LoadGeometry/LoadGeometry.h
@@ -5,20 +5,24 @@
 #include <iostream>
 
 #include "Tool.h"
+#include "Geometry.h"
 
 class LoadGeometry: public Tool {
 
 
  public:
 
-  LoadGeometry();
   bool Initialise(std::string configfile,DataModel &data);
   bool Execute();
   bool Finalise();
 
   bool FileExists(const std::string& name);
   std::string GetKeyLine(const std::string& name);
+  ConstructGeometry();
+  InitializeGeometry();
+  LoadFACCMRDDetectors();
 
+  Geometry* AnnieGeometry;
 
  private:
   std::string fFACCMRDGeoFile;
@@ -27,6 +31,21 @@ class LoadGeometry: public Tool {
   std::string fLAPPDGeoFile;
   std::string fDetectorGeoFile;
   
+  //Labels used in Geometry files to mark the legend and data entries
+  std::string LegendLineLabel = "LEGEND_LINE";
+  std::string DataStartLineLabel = "DATA_START";
+  std::string DataEndLineLabel = "DATA_END";
+
+
+  //Vector of strings indicating variables of interest and their data types in
+  //The MRD file.  Used in the LoadFACCMRDDetectors() method
+  std::string[20] MRDIntegerValues = {"detector_num","channel_num","detector_system","orientation","layer","side","num",
+                                   "rack","TDC slot","TDC channel","discrim_slot","discrim_ch",
+                                   "patch_panel_row","patch_panel_col","amp_slot","amp_channel",
+                                   "hv_crate","hv_slot","hv_channel","nominal_HV","polarity"};
+  std::string[6] MRDDoubleValues = {"x_center","y_center","z_center","x_width","y_width","z_width"};
+  std::string[3] MRDStringValues = {"PMT_type","cable_label","paddle_label"};
+
 	//verbosity levels: if 'verbosity' < this level, the message type will be logged.
 	int v_error=0;
 	int v_warning=1;
