b71cfb9dacd87d0851b8f9f7eeedcd402162dbee
stenderm <45366912+stenderm@users.noreply.github.com>
2019-08-16 14:39:48 +0200

    Return to default main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]){
 
   std::string conffile;
-  if (argc==1)conffile="configfiles/LoadGeometry/ToolChainConfig";
+  if (argc==1)conffile="configfiles/Dummy/ToolChainConfig";
   else conffile=argv[1];
 
   ToolChain tools(conffile);
