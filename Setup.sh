18d275790fbcd753e19a52c82708c8e945caba95
brichards64 <b.richards@qmul.ac.uk>
2019-08-28 23:10:02 +0100

    Root6, python 3 genie etc... (#137)
    * updated make files setup and python scripts for root 6 and python3
    
    * added base build and gettooldaq dependancies
    
    * added missing root6 flags
    
    * added dependancies and cahnged pip used
    
    * epdated setup to include genie and pythia paths
    
    * corrected mrd makefile
    
    * updated MRDmakefile switch
    
    * updated WCSim make
    
    * updated if logic bug
    
    * removed dockerfile.light and updated docs

--- Setup.sh
+++ Setup.sh
@@ -4,10 +4,13 @@
 
 ToolDAQapp=`pwd`
 
-source ${ToolDAQapp}/ToolDAQ/root/bin/thisroot.sh
+source ${ToolDAQapp}/ToolDAQ/root-6.06.08/install/bin/thisroot.sh
 
 export LD_LIBRARY_PATH=`pwd`/lib:${ToolDAQapp}/lib:${ToolDAQapp}/ToolDAQ/zeromq-4.0.7/lib:${ToolDAQapp}/ToolDAQ/boost_1_66_0/install/lib:${ToolDAQapp}/ToolDAQ/root/lib:${ToolDAQapp}/ToolDAQ/WCSimLib/:${ToolDAQapp}/ToolDAQ/MrdTrackLib/src:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=${ToolDAQapp}/ToolDAQ/WCSimLib/include/:${ToolDAQapp}/ToolDAQ/MrdTrackLib/include:$ROOT_INCLUDE_PATH
+
+export PYTHIA6_DIR=/ToolAnalysis/ToolDAQ/Pythia6Support/v6_424/
+ export GENIE=/ToolAnalysis/ToolDAQ/Generator-R-3_00_04/
 
 for folder in `ls -d ${ToolDAQapp}/UserTools/*/ `
 do
