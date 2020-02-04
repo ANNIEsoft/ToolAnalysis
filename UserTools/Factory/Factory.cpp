aae700657c3558ad705b2e4925b7f6f0da29f4b8
Michael Nieslony <michael.nieslony@gmail.com>
2020-02-04 04:29:53 -0600

    removed PrintADCData from Factory

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -98,6 +98,5 @@ if (tool=="MCHitToHitComparer") ret=new MCHitToHitComparer;
 if (tool=="PMTDataDecoder") ret=new PMTDataDecoder;
 if (tool=="ANNIEEventBuilder") ret=new ANNIEEventBuilder;
 if (tool=="MRDDataDecoder") ret=new MRDDataDecoder;
-if (tool=="PrintADCData") ret=new PrintADCData;
 return ret;
 }
