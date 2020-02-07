fe1fc811f332e202af46bfc5f7b750347ba8d3f3
brichards64 <b.richards@qmul.ac.uk>
2020-02-07 02:48:50 +0000

    Update Setup.sh
    removed duplicated lines

--- Setup.sh
+++ Setup.sh
@@ -12,9 +12,6 @@ export ROOT_INCLUDE_PATH=${ToolDAQapp}/ToolDAQ/WCSimLib/include/:${ToolDAQapp}/T
 export PYTHIA6_DIR=/ToolAnalysis/ToolDAQ/Pythia6Support/v6_424/
  export GENIE=/ToolAnalysis/ToolDAQ/Generator-R-3_00_04/
 
-export PYTHIA6_DIR=/ToolAnalysis/ToolDAQ/Pythia6Support/v6_424/
- export GENIE=/ToolAnalysis/ToolDAQ/Generator-R-3_00_04/
-
 for folder in `ls -d ${ToolDAQapp}/UserTools/*/ `
 do
     export PYTHONPATH=$folder:${PYTHONPATH}
