b8c5db69646a48a19dfee3af9de832f945d30a39
mnieslony <41126346+mnieslony@users.noreply.github.com>
2020-05-16 00:01:39 +0200

    Update MonitorReceive.cpp

--- UserTools/MonitorReceive/MonitorReceive.cpp
+++ UserTools/MonitorReceive/MonitorReceive.cpp
@@ -143,8 +143,9 @@ bool MonitorReceive::Execute(){
         	int EntriesToDo,CDEntryNum;
         	if (totalentries < 3000) EntriesToDo = 70;      //don't process as many waveforms for AmBe runs (typically ~ 1000 entries)
         	else EntriesToDo = (int) totalentries/15;               //otherwise do ~1000 entries out of 15000
-        	CDEntryNum = totalentries - EntriesToDo - 10;
-        	if (CDEntryNum < 0) CDEntryNum = 0;
+        	//CDEntryNum = totalentries - EntriesToDo - 10;
+        	//if (CDEntryNum < 0) CDEntryNum = 0;
+		CDEntryNum = 0;
         	std::map<int,std::vector<CardData>> CardData_Map;
         	while ((ExecuteEntryNum < EntriesToDo) && (CDEntryNum < totalentries)){
             		std::cout <<"ExecuteEntryNum: "<<ExecuteEntryNum<<std::endl;
