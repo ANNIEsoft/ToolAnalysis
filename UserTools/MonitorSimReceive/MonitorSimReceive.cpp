0406ad462e977d5ea96f426087056a40ae8bd34e
mnieslony <41126346+mnieslony@users.noreply.github.com>
2020-09-30 11:19:04 +0200

    Trigger Monitoring (#182)
    * Update README.md of MonitorReceive tool
    
    * added SimMonitoring toolchain for tests
    
    * Update MonitorSimReceive.cpp
    
    * Update PMTDataDecoder.cpp
    
    * Update MonitorReceive.cpp
    
    * Update MonitorReceive.cpp
    
    * Update MonitorReceive.cpp
    
    * Update ToolChainConfig
    
    * Update MonitorTankTimeConfig
    
    * Update MonitorReceive.cpp
    
    * Update MonitorSimReceive.cpp
    
    * Update MonitorMRDTimeConfig
    
    * Update PMTDataDecoderConfig
    
    * Update MonitorTankTime (correct computation of max/min values)
    
    * Update MonitorReceive.cpp
    
    * Removed verbose cout from PMTDataDecoder
    
    * Update MonitorReceive: log the incoming raw file names
    
    * Sort timestamps (MonitorTankTime)
    
    * Update MonitorReceive tool to loop over fixed amount of entries in the raw data stores.
    
    * Added framework for MonitorTrigger tool, fixed typo in MonitorReceive tool
    
    * Added configfiles for first working MonitorTrigger version
    
    * Update README.md of MonitorTrigger tool
    
    * Finalised first version of MonitorTrigger, added hitmaps & time clustering to MonitorTank
    
    * Reduced verbosity of MonitorTrigger config (5->0)
    
    * Add Reset for hist_tdc_cluster in MonitorMRDTime (previously missing)
    
    * Additional (previously missing) delete calls in case the file was already written to file.
    
    * Added delete calls in case the data was already written (MonitorTankTime)
    
    * Added delete calls in case the data was already written to file (MonitorTrigger tool)
    
    * added TDC cluster histogram integrated over 20 files
    
    * Reduced verbosity of MonitorReceive tool
    
    Co-authored-by: brichards64 <b.richards@qmul.ac.uk>

--- UserTools/MonitorSimReceive/MonitorSimReceive.cpp
+++ UserTools/MonitorSimReceive/MonitorSimReceive.cpp
@@ -44,9 +44,11 @@ bool MonitorSimReceive::Initialise(std::string configfile, DataModel &data){
     srand(time(NULL));
     m_data->Stores["CCData"]=new BoostStore(false,2);  
     m_data->Stores["PMTData"]=new BoostStore(false,2);
+    m_data->Stores["TrigData"]=new BoostStore(false,2);
 
     MRDData = 0;
     PMTData = 0;
+    TrigData = 0;
     indata = 0;
 
     i_loop = 0;
@@ -68,6 +70,10 @@ bool MonitorSimReceive::Initialise(std::string configfile, DataModel &data){
 bool MonitorSimReceive::Execute(){
 
     if (verbosity > 2) std::cout <<"MonitorSimReceive: Executing"<<std::endl;
+
+    m_data->CStore.Set("HasCCData",false);
+    m_data->CStore.Set("HasPMTData",false);
+    m_data->CStore.Set("HasTrigData",false);
 
     if (mode == "Wait"){
       std::string Wait = "Wait";     
@@ -104,6 +110,8 @@ bool MonitorSimReceive::Execute(){
     } else {
       m_data->vars.Set("StopLoop",true);
       if (indata!=0){ indata->Close(); indata->Delete(); delete indata; indata = 0;}
+      if (PMTData!=0) {PMTData->Close(); PMTData->Delete(); delete PMTData; PMTData = 0;}
+      if (TrigData!=0) {TrigData->Close(); TrigData->Delete(); delete TrigData; TrigData = 0;}
       return true;
     }
 
@@ -112,6 +120,17 @@ bool MonitorSimReceive::Execute(){
     }
     if (PMTData!=0){
       m_data->Stores["PMTData"]->Delete();
+      PMTData->Close();
+      PMTData->Delete();
+      delete PMTData;
+      PMTData=0;
+    }
+    if (TrigData!=0){
+      m_data->Stores["TrigData"]->Delete();
+      TrigData->Close();
+      TrigData->Delete();
+      delete TrigData;
+      TrigData=0;
     }
     if (indata!=0){
       std::cout <<"close indata"<<std::endl;
@@ -131,6 +150,7 @@ bool MonitorSimReceive::Execute(){
 
     bool has_cc=false;
     bool has_pmt=false;
+    bool has_trig=false;
 
     if (indata->Has("CCData")){
 	std::cout <<"RawData has CCData Store!"<<std::endl;
@@ -142,9 +162,15 @@ bool MonitorSimReceive::Execute(){
         has_pmt = true;
         PMTData = new BoostStore(false,2);
     }
+    if (indata->Has("TrigData")){
+        std::cout <<"RawData has TrigData Store!"<<std::endl;
+        has_trig = true;
+        TrigData = new BoostStore(false,2);
+    }
 
     m_data->CStore.Set("HasCCData",has_cc);
     m_data->CStore.Set("HasPMTData",has_pmt);
+    m_data->CStore.Set("HasTrigData",has_trig);
     std::string State="DataFile";
     m_data->CStore.Set("State",State);
 
@@ -156,8 +182,41 @@ bool MonitorSimReceive::Execute(){
 
     if (has_pmt){
         indata->Get("PMTData",*PMTData);
-        PMTData->Save("tmp");
-        m_data->Stores["PMTData"]->Set("FileData",PMTData,false);
+        //PMTData->Save("tmp");
+	PMTData->Print(false);
+        long totalentries;
+        PMTData->Header->Get("TotalEntries",totalentries);
+        std::cout <<"MonitorSimReceive: Total entries: "<<totalentries<<std::endl;
+        int ExecuteEntryNum=0;
+        int EntriesToDo,CDEntryNum;
+        if (totalentries < 3000) EntriesToDo = 70;      //don't process as many waveforms for AmBe runs (typically ~ 1000 entries)
+        else EntriesToDo = (int) totalentries/15;               //otherwise do ~1000 entries out of 15000
+        CDEntryNum = 0;
+	std::map<int,std::vector<CardData>> CardData_Map;
+        while ((ExecuteEntryNum < EntriesToDo) && (CDEntryNum < totalentries)){
+         //   std::cout <<"ExecuteEntryNum: "<<ExecuteEntryNum<<std::endl;
+            std::vector<CardData> vector_CardData;
+            PMTData->GetEntry(CDEntryNum);
+            PMTData->Get("CardData",vector_CardData);
+            CardData_Map.emplace(CDEntryNum,vector_CardData);
+            ExecuteEntryNum++;
+            CDEntryNum++;
+        }
+        m_data->Stores["PMTData"]->Set("CardDataMap",CardData_Map);  
+    }
+
+    if (has_trig){
+      indata->Get("TrigData",*TrigData);
+      long totalentries_trig;
+      TrigData->Header->Get("TotalEntries",totalentries_trig);
+      std::map<int,TriggerData> TrigData_Map;
+      for (int i_trig=0; i_trig < totalentries_trig; i_trig++){
+        TriggerData TData;
+        TrigData->GetEntry(i_trig);
+        TrigData->Get("TrigData",TData);
+        TrigData_Map.emplace(i_trig,TData);
+      }
+      m_data->Stores["TrigData"]->Set("TrigDataMap",TrigData_Map);
     }
 
     i_loop++;
@@ -172,14 +231,20 @@ bool MonitorSimReceive::Finalise(){
     if (verbosity > 2) std::cout <<"MonitorSimReceive: Finalising"<<std::endl;
 
     if (indata!=0){ indata->Close(); indata->Delete(); delete indata; indata = 0;}
+    if (PMTData!=0) {PMTData->Close(); PMTData->Delete(); delete PMTData; PMTData = 0;}
+    if (TrigData!=0) {TrigData->Close(); TrigData->Delete(); delete TrigData; TrigData = 0;}
 
     if (m_data->CStore.Has("State")) m_data->CStore.Remove("State");
     if (m_data->CStore.Has("HasCCData")) m_data->CStore.Remove("HasCCData");
     if (m_data->CStore.Has("HasPMTData")) m_data->CStore.Remove("HasPMTData");
+    if (m_data->CStore.Has("HasTrigData")) m_data->CStore.Remove("HasTrigData");
     if (m_data->Stores["CCData"]->Has("FileData")) m_data->Stores["CCData"]->Remove("FileData");
     if (m_data->Stores["PMTData"]->Has("FileData")) m_data->Stores["PMTData"]->Remove("FileData");
+    if (m_data->Stores["PMTData"]->Has("CardDataMap")) m_data->Stores["PMTData"]->Remove("CardDataMap");
+    if (m_data->Stores["TrigData"]->Has("TrigDataMap")) m_data->Stores["TrigData"]->Remove("TrigDataMap");
     m_data->Stores["CCData"]->Close(); m_data->Stores["CCData"]->Delete(); 
     m_data->Stores["PMTData"]->Close(); m_data->Stores["PMTData"]->Delete(); 
+    m_data->Stores["TrigData"]->Close(); m_data->Stores["TrigData"]->Delete();
     m_data->Stores.clear();
 
     return true;
