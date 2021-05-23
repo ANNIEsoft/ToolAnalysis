54a8c81f133a56e9a79656f80c8a9c211a1fc8d9
Marcus O'Flaherty <msoflaherty1@sheffield.ac.uk>
2021-05-23 21:51:35 +0100

    update python api, fixing bug where multiple tools could not import the same module. Give python tools their own config file Store. Add a generic python GetStoreVariable function to simplify retrieval of basic datatypes. Add a minimal 'FakePython' toolchain for testing.

--- DataModel/ANNIEconstants.h
+++ DataModel/ANNIEconstants.h
@@ -2,6 +2,9 @@
 #define ANNIECONSTANTS_H
 
 #include <math.h>
+#include <regex>
+#include <boost/regex.hpp> // since std::regex doesn't work
+#include <boost/regex/pattern_except.hpp>
 
 /// @brief The impedance (in Ohms) to assume when computing charge values for
 /// calibrated ADC hits
@@ -27,5 +30,41 @@ constexpr int BOOST_STORE_MULTIEVENT_FORMAT = 2;
 constexpr unsigned long long THOUSAND = 1000ull;
 constexpr unsigned long long MILLION = 1000000ull;
 constexpr unsigned long long BILLION = 1000000000ull;
+
+// constants for mapping regular expression error codes to descriptive strings
+// ---------------------------------------------------------------------------
+const std::map<std::regex_constants::error_type,std::string> regex_err_strings{
+	{std::regex_constants::error_collate, "The expression contained an invalid collating element name."},
+	{std::regex_constants::error_ctype, "The expression contained an invalid character class name."},
+	{std::regex_constants::error_escape, "The expression contained an invalid escaped character, or a trailing escape."},
+	{std::regex_constants::error_backref, "The expression contained an invalid back reference."},
+	{std::regex_constants::error_brack, "The expression contained mismatched brackets ([ and ])."},
+	{std::regex_constants::error_paren, "The expression contained mismatched parentheses (( and ))."},
+	{std::regex_constants::error_brace, "The expression contained mismatched braces ({ and })."},
+	{std::regex_constants::error_badbrace, "The expression contained an invalid range between braces ({ and })."},
+	{std::regex_constants::error_range, "The expression contained an invalid character range."},
+	{std::regex_constants::error_space, "There was insufficient memory to convert the expression into a finite state machine."},
+	{std::regex_constants::error_badrepeat, "The expression contained a repeat specifier (one of *?+{) that was not preceded by a valid regular expression."},
+	{std::regex_constants::error_complexity, "The complexity of an attempted match against a regular expression exceeded a pre-set level."},
+	{std::regex_constants::error_stack, "There was insufficient memory to determine whether the regular expression could match the specified character sequence."}
+};
+
+// boost version
+const std::map<boost::regex_constants::error_type,std::string> bregex_err_strings{
+	{boost::regex_constants::error_collate, "The expression contained an invalid collating element name."},
+	{boost::regex_constants::error_ctype, "The expression contained an invalid character class name."},
+	{boost::regex_constants::error_escape, "The expression contained an invalid escaped character, or a trailing escape."},
+	{boost::regex_constants::error_backref, "The expression contained an invalid back reference."},
+	{boost::regex_constants::error_brack, "The expression contained mismatched brackets ([ and ])."},
+	{boost::regex_constants::error_paren, "The expression contained mismatched parentheses (( and ))."},
+	{boost::regex_constants::error_brace, "The expression contained mismatched braces ({ and })."},
+	{boost::regex_constants::error_badbrace, "The expression contained an invalid range between braces ({ and })."},
+	{boost::regex_constants::error_range, "The expression contained an invalid character range."},
+	{boost::regex_constants::error_space, "There was insufficient memory to convert the expression into a finite state machine."},
+	{boost::regex_constants::error_badrepeat, "The expression contained a repeat specifier (one of *?+{) that was not preceded by a valid regular expression."},
+	{boost::regex_constants::error_complexity, "The complexity of an attempted match against a regular expression exceeded a pre-set level."},
+	{boost::regex_constants::error_stack, "There was insufficient memory to determine whether the regular expression could match the specified character sequence."},
+	{boost::regex_constants::error_bad_pattern, "Invalid regex pattern."}
+};
 
 #endif
--- UserTools/PythonScript/PythonScript.cpp
+++ UserTools/PythonScript/PythonScript.cpp
@@ -21,11 +21,18 @@ bool PythonScript::Initialise(std::string configfile, DataModel &data){
   m_variables.Get("InitialiseFunction",initialisefunction);
   m_variables.Get("ExecuteFunction",executefunction);
   m_variables.Get("FinaliseFunction",finalisefunction);
+  std::string scriptconfigfile="";
+  m_variables.Get("ConfigurationsFile",scriptconfigfile);
 
-  gstore=m_data->Stores["DataName"];
-
+  // Initialize a Store with the python script's variables
+  thisscriptsconfigstore.Initialise(scriptconfigfile);
 
   PyImport_AppendInittab("Store", test);
+  
+  gstore=m_data;
+  // each time we're running this python script, set the global gconfig to point to it's configurations store
+  // so that calls to the PythonAPI for config values will return values from the correct store.
+  gconfig=&thisscriptsconfigstore;
 
   // Initialising Python
   pyinit=0;
@@ -38,8 +45,8 @@ bool PythonScript::Initialise(std::string configfile, DataModel &data){
   m_data->CStore.Set("PythonInit",pyinit);
   
   /// Starting python thread for this tool
-  pythread=Py_NewInterpreter();  
-  PyThreadState_Swap(pythread);
+  //pythread=Py_NewInterpreter();  
+  //PyThreadState_Swap(pythread);
 
   // Loading store API into python env
   //Py_InitModule("Store", StoreMethods); ///python 2 version
@@ -68,11 +75,12 @@ bool PythonScript::Initialise(std::string configfile, DataModel &data){
     pFuncF = PyObject_GetAttrString(pModule, finalisefunction.c_str());
 
     if (pFuncI && pFuncE && pFuncF && PyCallable_Check(pFuncI) && PyCallable_Check(pFuncE) && PyCallable_Check(pFuncF)) {
-      pArgs = PyTuple_New(0);
+      //pArgs = PyTuple_New(0);
+       pArgs = Py_BuildValue("(i)",pyinit);
       
-      /* we dont rreally want to pass any arguments but have the option here
+      /* we dont really want to pass any arguments but have the option here
 	 for (int i = 0; i < 0; ++i) {
-	 pValue = PyInt_FromLong(i);
+	 pValue = PyLong_FromLong(i);
 	 if (!pValue) {
 	 Py_DECREF(pArgs);
 	 Py_DECREF(pModule);
@@ -90,7 +98,7 @@ bool PythonScript::Initialise(std::string configfile, DataModel &data){
       
       
       if(pValue != NULL){
-	if (!(PyLong_AsLong(pValue))){ 
+	if (!(PyLong_AsLong(pValue))){
 	  std::cout<<"Python script returned internal error in initialise "<<std::endl;	
 	  return false;
 	}
@@ -131,7 +139,10 @@ bool PythonScript::Initialise(std::string configfile, DataModel &data){
 
 bool PythonScript::Execute(){
 
-  PyThreadState_Swap(pythread);
+  // make the config Store for this script accessible to the tool, should it need it in Execute
+  gconfig=&thisscriptsconfigstore;
+
+  //PyThreadState_Swap(pythread);
 
   if (pModule != NULL) {
 
@@ -173,8 +184,11 @@ bool PythonScript::Execute(){
 
 
 bool PythonScript::Finalise(){
+
+  // make the config Store for this script accessible to the tool, should it need it in Finalise
+  gconfig=&thisscriptsconfigstore;
   
-  PyThreadState_Swap(pythread);  
+  //PyThreadState_Swap(pythread);  
   
   if (pModule != NULL) {
     
