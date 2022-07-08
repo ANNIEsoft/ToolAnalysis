2681882f7969ac6706ce4cc9c3092a9d96747c89
Matt Wetstein <wetstein@zh-a426-mbp.physics.iastate.edu>
2022-07-08 00:05:30 -0500

    made flexible over more than two boards and backwards compatible with old dta

--- UserTools/LAPPDFindT0/LAPPDFindT0.h
+++ UserTools/LAPPDFindT0/LAPPDFindT0.h
@@ -19,31 +19,33 @@
 class LAPPDFindT0: public Tool {
 
 
- public:
+    public:
 
-  LAPPDFindT0(); ///< Simple constructor
-  bool Initialise(std::string configfile,DataModel &data); ///< Initialise Function for setting up Tool resources. @param configfile The path and name of the dynamic configuration file to read in. @param data A reference to the transient data class used to pass information between Tools.
-  bool Execute(); ///< Execute function used to perform Tool purpose.
-  bool Finalise(); ///< Finalise function used to clean up resources.
-  double Tfit(std::vector<double>* wf);
+        LAPPDFindT0(); ///< Simple constructor
+        bool Initialise(std::string configfile,DataModel &data); ///< Initialise Function for setting up Tool resources. @param configfile The path and name of the dynamic configuration file to read in. @param data A reference to the transient data class used to pass information between Tools.
+        bool Execute(); ///< Execute function used to perform Tool purpose.
+        bool Finalise(); ///< Finalise function used to clean up resources.
+        double Tfit(std::vector<double>* wf);
 
- private:
+    private:
 
-   int T0offset;
-   double T0signalmax,T0signalthreshold;
-   int T0channelNo;
-   int FindT0VerbosityLevel;
-   int globalshiftT0;
-   string InputWavLabel;
-   string OutputWavLabel;
-   int trigearlycut,triglatecut;
+        //vector for save
+        vector<bool> vec_T0signalInWindow;
+        vector<double> vec_deltaT;
+        vector<int> vec_T0Bin;
 
-   //New
-   int TrigChannel;
-   int LAPPDchannelOffset;
-
-
-
+        int LAPPDchannelOffset;
+        int TrigChannel;
+        int T0offset;
+        double T0signalmax,T0signalthreshold;
+        double T0signalmaxOld,T0signalthresholdOld;
+        int T0channelNo;
+        int FindT0VerbosityLevel;
+        int globalshiftT0;
+        string InputWavLabel;
+        string OutputWavLabel;
+        int trigearlycut,triglatecut;
+        int oldLaser;
 };
 
 
