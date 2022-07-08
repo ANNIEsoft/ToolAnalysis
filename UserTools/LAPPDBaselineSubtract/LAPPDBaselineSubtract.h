8807bdfc5da51193dc6655fd95b90eb232bf09bc
Matt Wetstein <wetstein@zh-a426-mbp.physics.iastate.edu>
2022-07-08 18:12:48 -0500

    deleted old LAPPDBaselineSubtract and renamed LAPPDStoreBaselineSubtract to LAPPDBaselineSubtract

--- UserTools/LAPPDBaselineSubtract/LAPPDBaselineSubtract.h
+++ UserTools/LAPPDBaselineSubtract/LAPPDBaselineSubtract.h
@@ -11,30 +11,29 @@
 class LAPPDBaselineSubtract: public Tool {
 
 
- public:
+    public:
 
-  LAPPDBaselineSubtract();
-  bool Initialise(std::string configfile,DataModel &data);
-  bool Execute();
-  bool Finalise();
+        LAPPDBaselineSubtract();
+        bool Initialise(std::string configfile,DataModel &data);
+        bool Execute();
+        bool Finalise();
 
 
- private:
+    private:
 
-   Waveform<double> SubtractSine(Waveform<double> iwav);
-   bool isSim;
-   int DimSize;
-   int TrigChannel1;
-   int TrigChannel2;
-   double Deltat;
-   double LowBLfitrange;
-   double HiBLfitrange;
-   double TrigLowBLfitrange;
-   double TrigHiBLfitrange;
-   double oldLaserTrigAmpRange;
-   int oldLaser;
-   string BLSInputWavLabel;
-   string BLSOutputWavLabel;
+        Waveform<double> SubtractSine(Waveform<double> iwav);
+        int LAPPDchannelOffset;
+        int BLSVerbosityLevel;
+        bool isSim;
+        int DimSize;
+        int TrigChannel;
+        double Deltat;
+        double LowBLfitrange;
+        double HiBLfitrange;
+        double TrigLowBLfitrange;
+        double TrigHiBLfitrange;
+        string BLSInputWavLabel;
+        string BLSOutputWavLabel;
 
 };
 
