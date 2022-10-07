990c6355598d50913bfe406b083125f5e5b7e62b
marc1uk <marc1uk_@hotmail.com>
2022-10-08 00:23:24 +0100

    update python api, fixing bug where multiple tools could not import the same module. Give python tools their own config file Store. Add a generic python GetStoreVariable function to simplify retrieval of basic datatypes. Add a minimal 'FakePython' toolchain for testing. (#191)
    Co-authored-by: Marcus O'Flaherty <msoflaherty1@sheffield.ac.uk>

---
+++ UserTools/FakePythonTool/my_fake_tool.py
@@ -0,0 +1,16 @@
+import Store  # i don't believe this causes problems as it's defined in ToolAnalysis
+import numpy as np
+import tensorflow as tf
+
+def Initialise(pyinit):
+    print("i've initialized")
+    return 1
+
+def Execute():
+    print("i'm executing")
+    return 1
+
+def Finalise():
+    print("i've finalised")
+    return 1
+
