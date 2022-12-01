8eb886c2c91fdc496af5a040c0dd957767adb965
Michael Nieslony <41126346+mnieslony@users.noreply.github.com>
2022-12-01 13:29:37 +0100

    Update LoadWCSimLAPPD
    small bugfix

--- UserTools/LoadWCSimLAPPD/LoadWCSimLAPPD.cpp
+++ UserTools/LoadWCSimLAPPD/LoadWCSimLAPPD.cpp
@@ -165,7 +165,7 @@ bool LoadWCSimLAPPD::Execute(){
 	
 	bool splitSubtriggers = false;
 	m_data->CStore.Get("SplitSubTriggers",splitSubtriggers);
-	if((splitSubtriggers && (MCTriggernum>0)) || (!splitSubtriggers)){
+	if((splitSubtriggers && (MCTriggernum>0))){
 		if(unassignedhits.size()==0){
 			if(verbosity>2) cout<<"no LAPPD hits to add to this trigger"<<endl;
 			return true;
