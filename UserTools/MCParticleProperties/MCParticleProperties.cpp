99736d09e0dcb6f80f7d20cfb80c3f1ab45e1723
Michael Nieslony <41126346+mnieslony@users.noreply.github.com>
2023-01-18 16:59:36 +0100

    LoadWCSim Update (#202)
    * add LoadWCSim updates
    * udpated config file
    * updated MCParticleProperties tool to be in line with LoadWCSim updates
    * checks on channel map
    * enable use of default if one not provided by user
    * check file exists in either case.
    * added a toggle to split triggers in LoadWCSim
    * added correct channelkey maps for WCSim
    * Update LoadWCSimLAPPD
    * small bugfix
    * error messages
    * relative neutron capture times
    * rework of the Geometry class

--- UserTools/MCParticleProperties/MCParticleProperties.cpp
+++ UserTools/MCParticleProperties/MCParticleProperties.cpp
@@ -58,10 +58,15 @@ bool MCParticleProperties::Execute(){
 	// on this set of particles and we don't need to do it again
 	uint16_t MCTriggernum;
 	get_ok = m_data->Stores["ANNIEEvent"]->Get("MCTriggernum",MCTriggernum);
+	Log("MCParticleProperties tool: MCTriggernum: "+std::to_string(MCTriggernum),v_message,verbosity);
 	if(not get_ok){
 		Log("MCParticleProperties Tool: No MCTriggernum in ANNIEEvent!",v_error,verbosity);
 		return false;
-	} else if(MCTriggernum>0){
+	}
+
+	bool splitSubtriggers = false;
+        m_data->CStore.Get("SplitSubTriggers",splitSubtriggers);
+	if(splitSubtriggers && (MCTriggernum>0)){
 		return true; // nothing to do
 	}
 	
