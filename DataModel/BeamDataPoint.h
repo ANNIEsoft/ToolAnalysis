3351011b901df897a44f23392cbf44c7d12cdd46
Andrew Sutton <asutton@anniegpvm01.fnal.gov>
2024-03-07 08:29:57 -0600

    Add versioning for backward compatibility

--- DataModel/BeamDataPoint.h
+++ DataModel/BeamDataPoint.h
@@ -18,26 +18,39 @@ struct BeamDataPoint : public SerialisableObject {
 
   friend class boost::serialization::access;
 
-  BeamDataPoint() : value(0.), unit(""), time(0) {}
-  BeamDataPoint(double Value, const std::string& Unit, uint64_t time=0)
-    : value(Value), unit(Unit), time(0) {}
+  BeamDataPoint() : value(0.), unit(""), time(1) {}
+  BeamDataPoint(double Value, const std::string& Unit, uint64_t time=1)
+    : value(Value), unit(Unit), time(time) {
+    std::cout << "BDP - value: " << value << ", unit: " << unit << ", time: " << time << std::endl;}
   double value;
   std::string unit;
   uint64_t time;
-
+    
   template<class Archive> void serialize(Archive & ar,
     const unsigned int version)
   {
     ar & value;
     ar & unit;
-    ar & time;
+    if (version > 0)
+      ar & time;
   }
 
   virtual bool Print() override {
     std::cout << "Value : " << value << '\n';
-    std::cout << "Unit  : " << unit  << '\n';
+    std::cout << "Unit  : " << unit  << '\n'
     std::cout << "Time  : " << time  << '\n';
     return true;
   }
 
 };
+
+// Need to increment the class version since we added time as a new variable
+// the version number ensures backward compatibility when serializing 
+BOOST_CLASS_VERSION(BeamDataPoint, 1)
+
+
+
+
+
+
+
