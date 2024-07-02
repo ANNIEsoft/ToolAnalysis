ca2e58e3b298888a37d8c03d6495230dd8aa9ab2
Marcus O'Flaherty <moflaher@fnal.gov>
2024-07-02 18:56:14 -0500

    further pruning of unnecessary files

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -5,13 +5,6 @@ Tool* ret=0;
 
 // if (tool=="Type") tool=new Type;
 if (tool=="DummyTool") ret=new DummyTool;
-if (tool=="ExampleGenerateData") ret=new ExampleGenerateData;
-if (tool=="ExampleSaveStore") ret=new ExampleSaveStore;
-if (tool=="ExampleSaveRoot") ret=new ExampleSaveRoot;
-if (tool=="ExampleloadStore") ret=new ExampleloadStore;
-if (tool=="ExamplePrintData") ret=new ExamplePrintData;
-if (tool=="ExampleLoadRoot") ret=new ExampleLoadRoot;
-if (tool=="ExampleOverTool") ret=new ExampleOverTool;
 if (tool=="MonitorReceive") ret=new MonitorReceive;
 if (tool=="MonitorSimReceive") ret=new MonitorSimReceive;
 if (tool=="MonitorMRDTime") ret=new MonitorMRDTime;
