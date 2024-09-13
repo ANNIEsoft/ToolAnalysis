3d29e936be92970c944d80de8137cc663e6be42f
Steven Doran <78985334+S81D@users.noreply.github.com>
2024-09-13 10:09:42 -0500

    Update ClusterFinder.h
    initialize config variable for pulse width

--- UserTools/ClusterFinder/ClusterFinder.h
+++ UserTools/ClusterFinder/ClusterFinder.h
@@ -67,6 +67,7 @@ class ClusterFinder: public Tool {
   int MinHitsPerCluster;
   bool draw_2D = false;
   double end_of_window_time_cut;
+  double pulse_width;
 
   // define ANNIEEvent variables
   int evnum;
