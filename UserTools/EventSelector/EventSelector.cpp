71b358a1a0960efe4e35ced3ca227f4797595376
rory42edwards <rory42edwards@gmail.com>
2024-11-11 16:29:53 +0000

    Set StopLoop to 1 when failed to get TriggerWord

--- UserTools/EventSelector/EventSelector.cpp
+++ UserTools/EventSelector/EventSelector.cpp
@@ -45,6 +45,7 @@ bool EventSelector::Initialise(std::string configfile, DataModel &data){
   bool got_trigger_word = m_variables.Get("TriggerWord",fTriggerWord);
   if (!got_trigger_word) {
       Log("EventSelector Tool: Woah there, buddy! We set TriggerWord around these parts. Come back when you've done that. \n If you're not sure what they might need to be, check out Table 3.6 in Michael Nieslony's thesis.");
+      m_data->vars.Set("StopLoop",1);
       return false;
   }
   m_variables.Get("SaveStatusToStore", fSaveStatusToStore);
