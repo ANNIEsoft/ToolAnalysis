1377e0ecaf1e4b01472a25e7e5b36dced9242d55
Steven Doran <78985334+S81D@users.noreply.github.com>
2024-11-14 12:24:38 -0600

    ANNIEEventTreeMaker Config file creation scripts
    Adjusted the python scripts that generate the Config file to include run type, avoiding the need to have separate toolchains for different run types

--- configfiles/BeamClusterAnalysis/configLAPPD.py
+++ configfiles/BeamClusterAnalysis/configLAPPD.py
@@ -3,6 +3,7 @@ import sys
 run = sys.argv[1]
 pi = sys.argv[2]
 pf = sys.argv[3]
+run_type = sys.argv[4]
 
 file = open('ANNIEEventTreeMakerConfig', "w")
 
@@ -10,17 +11,28 @@ file.write('ANNIEEventTreeMakerVerbosity 0\n')
 file.write('\n')
 file.write('fillAllTriggers 1\n')
 file.write('fill_singleTrigger 0\n')
+file.write('fill_singleTriggerWord 0\n')
 file.write('fillLAPPDEventsOnly 0\n')
 file.write('TankCluster_fill 1\n')
 file.write('cluster_TankHitInfo_fill 1\n')
 file.write('\n')
 file.write('OutputFile BeamCluster_LAPPD_' + str(run) + '_' + str(pi) + '_' + str(pf) + '.ntuple.root\n')
 file.write('TankClusterProcessing 1\n')
-file.write('MRDClusterProcessing 1\n')
 file.write('TriggerProcessing 1\n')
 file.write('TankHitInfo_fill 1\n')
-file.write('MRDHitInfo_fill 1\n')
-file.write('MRDReco_fill 1\n')
+file.write('\n')
+
+# if we are running the LAPPDs at this stage, it is either a laser or beam run. Only beam runs need MRD info
+if run_type == 'beam':
+    file.write('MRDClusterProcessing 1\n')
+    file.write('MRDHitInfo_fill 1\n')
+    file.write('MRDReco_fill 1\n')
+else:
+    file.write('MRDClusterProcessing 0\n')
+    file.write('MRDHitInfo_fill 0\n')
+    file.write('MRDReco_fill 0\n')
+file.write('\n')
+
 file.write('SiPMPulseInfo_fill 0\n')
 file.write('fillCleanEventsOnly 0\n')
 file.write('MCTruth_fill 0\n')
@@ -28,12 +40,19 @@ file.write('Reco_fill 1\n')
 file.write('TankReco_fill 0\n')
 file.write('RecoDebug_fill 0\n')
 file.write('muonTruthRecoDiff_fill 0\n')
-file.write('IsData 1\n')
+file.write('isData 1\n')
 file.write('HasGenie 0')
+file.write('LAPPD_MC_fill 0\n')
 file.write('\n')
+
 file.write('LAPPDData_fill 1\n')
 file.write('LAPPDReco_fill 1\n')
-file.write('RWMBRF_fill 1\n')
 file.write('LAPPD_PPS_fill 1\n')
+file.write('LAPPD_Waveform_fill 1\n')
+
+if run_type == 'beam':
+    file.write('RWMBRF_fill 1\n')
+else:
+    file.write('RWMBRF_fill 0\n')
 
 file.close()
