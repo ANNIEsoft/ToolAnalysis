dbdf16baa3bfbf6f521c6337acb866a8360c6d60
fengyvoid <fengyvoid@gmail.com>
2025-04-16 20:30:46 -0500

    Add PMT type load from channel class to tree maker

--- DataModel/Channel.h
+++ DataModel/Channel.h
@@ -14,8 +14,9 @@ class Channel : public SerialisableObject{
 	public:
 	
 	Channel() : ChannelID(-1), channelrelposition(Position(-1,-1,-1)), stripside(2), stripnum(-1), signal_crate(-1), signal_card(-1), signal_channel(-1), level2_crate(-1), level2_card(-1), level2_channel(-1), hv_crate(-1), hv_card(-1), hv_channel(-1), status(channelstatus::OFF)
-	 {serialise=true;}
-	Channel(unsigned long chnID, Position chanpos, int strp, int strpnm, unsigned int sig_crt, unsigned int sig_crd, unsigned int sig_chn, unsigned int lv2_crt, unsigned int lv2_crd, unsigned int lv2_ch, unsigned int hv_crt, unsigned int hv_crd, unsigned int hv_chn, channelstatus chanstat) : ChannelID(chnID), channelrelposition(chanpos), stripside(strp), stripnum(strpnm), signal_crate(sig_crt), signal_card(sig_crd), signal_channel(sig_chn), level2_crate(lv2_crt), level2_card(lv2_crd), level2_channel(lv2_ch), hv_crate(hv_crt), hv_card(hv_crd), hv_channel(hv_chn), status(chanstat) {serialise=true;}
+	 {channelType = 0; serialise=true;}
+	Channel(unsigned long chnID, Position chanpos, int strp, int strpnm, unsigned int sig_crt, unsigned int sig_crd, unsigned int sig_chn, unsigned int lv2_crt, unsigned int lv2_crd, unsigned int lv2_ch, unsigned int hv_crt, unsigned int hv_crd, unsigned int hv_chn, channelstatus chanstat) : ChannelID(chnID), channelrelposition(chanpos), stripside(strp), stripnum(strpnm), signal_crate(sig_crt), signal_card(sig_crd), signal_channel(sig_chn), level2_crate(lv2_crt), level2_card(lv2_crd), level2_channel(lv2_ch), hv_crate(hv_crt), hv_card(hv_crd), hv_channel(hv_chn), status(chanstat) {channelType=0; serialise=true;}
+	Channel(unsigned long chnID, Position chanpos, int strp, int strpnm, unsigned int sig_crt, unsigned int sig_crd, unsigned int sig_chn, unsigned int lv2_crt, unsigned int lv2_crd, unsigned int lv2_ch, unsigned int hv_crt, unsigned int hv_crd, unsigned int hv_chn, channelstatus chanstat, int channelPMTType) : ChannelID(chnID), channelrelposition(chanpos), stripside(strp), stripnum(strpnm), signal_crate(sig_crt), signal_card(sig_crd), signal_channel(sig_chn), level2_crate(lv2_crt), level2_card(lv2_crd), level2_channel(lv2_ch), hv_crate(hv_crt), hv_card(hv_crd), hv_channel(hv_chn), status(chanstat), channelType(channelPMTType) {serialise=true;}
 	virtual ~Channel(){}
 	
 	unsigned long GetChannelID(){return ChannelID;}
@@ -32,6 +33,7 @@ class Channel : public SerialisableObject{
 	unsigned int GetHvCard(){return hv_card;}
 	unsigned int GetHvChannel(){return hv_channel;}
 	channelstatus GetStatus(){return status;}
+	int GetChannelType(){return channelType;}
 	
 	void SetChannelID(unsigned long channelIDin){ChannelID=channelIDin;}
 	void SetRelPos(Position pos){ channelrelposition=pos;}
@@ -47,6 +49,7 @@ class Channel : public SerialisableObject{
 	void SetHvCard(unsigned int cardin){hv_card=cardin;}
 	void SetHvChannel(unsigned int chanin){hv_channel=chanin;}
 	void SetStatus(channelstatus stat){status=stat;}
+	void SetChannelType(int type){channelType=type;}
 	
 	bool Print(){
 		std::cout<<"ChannelID          : "<<ChannelID<<std::endl;
@@ -81,6 +84,7 @@ class Channel : public SerialisableObject{
 	unsigned int signal_crate, signal_card, signal_channel, level2_crate, level2_card, level2_channel,
 	hv_crate, hv_card, hv_channel;
 	channelstatus status;
+	int channelType; // 0 = others, 1 = LUX, 2 = ETEL, 3 = Hamamatsu, 4 = Watchboy, 5 = Watchman
 	template<class Archive> void serialize(Archive & ar, const unsigned int	version){
 		if(serialise){
 			ar & ChannelID;
@@ -97,6 +101,7 @@ class Channel : public SerialisableObject{
 			ar & hv_card;
 			ar & hv_channel;
 			ar & status;
+			ar & channelType;
 		}
 	}
 	
