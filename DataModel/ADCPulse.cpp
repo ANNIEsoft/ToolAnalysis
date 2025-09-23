2eabe6cd679d5c7955ac9afd99e23044c5f0835c
Steven Doran <78985334+S81D@users.noreply.github.com>
2025-09-23 15:08:11 -0500

    Update ADCPulse.cpp
    Expand ADCpulse class to include entire trace

--- DataModel/ADCPulse.cpp
+++ DataModel/ADCPulse.cpp
@@ -10,6 +10,7 @@ ADCPulse::ADCPulse(int TubeId, double start_time, double peak_time,
   double charge) : Hit(TubeId, start_time, charge),
   start_time_(start_time), peak_time_(peak_time),
   baseline_(baseline), sigma_baseline_(sigma_baseline), raw_area_(area),
-  raw_amplitude_(raw_amplitude), calibrated_amplitude_(calibrated_amplitude)
+  raw_amplitude_(raw_amplitude), calibrated_amplitude_(calibrated_amplitude),
+  trace_x_(x_points), trace_y_(y_points)
 {
 }
