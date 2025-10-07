00db06452ba0697b3283770df3ea3d6361f3826f
Steven Doran <78985334+S81D@users.noreply.github.com>
2025-10-07 11:34:55 -0500

    Update Factory.cpp
    Add PrintADCTraces to factory

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -176,5 +176,6 @@ if (tool=="AssignBunchTimingMC") ret=new AssignBunchTimingMC;
 if (tool=="FitRWMWaveform") ret=new FitRWMWaveform;
 if (tool=="PMTWaveformSim") ret=new PMTWaveformSim;
 if (tool=="LAPPDWaveformDisplay") ret=new LAPPDWaveformDisplay;
+if (tool=="PrintADCTraces") ret=new PrintADCTraces;
 return ret;
 }
